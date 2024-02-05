12955b61e04c03c1206a503f07593e954196683e
rutwik121 <rutwik121@gmail.com>
2024-02-05 11:28:12 +0530

    check_script

--- git_push.sh
+++ git_push.sh
@@ -5,5 +5,5 @@ read -p "Enter a commit message  :  " commit_msg_var
 echo $commit_msg_var
 
 git add .
-git commit -m '$commit_msg_var' # Update this text with your own message
+git commit -m $commit_msg_var # Update this text with your own message
 git push
